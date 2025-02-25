adbd4c3841588f461a99d7a81a8c4133f64ffc22
Victor Lyashenko <kenuay007@gmail.com>
2025-02-25 15:58:38 +0300

    Init

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+// Response структура для JSON-ответа
+type Response struct {
+	CurrentTime string `json:"current_time"`
+}
+
+func currentTimeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	now := time.Now()
+	response := Response{
+		CurrentTime: now.Format(time.RFC3339), // Форматирование времени в ISO 8601
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("GET /current-time", currentTimeHandler) // Обработчик для GET /current-time
+
+	// Запуск сервера
+	log.Fatalf("Server error: %v", http.ListenAndServe(":8080", nil))
+}
